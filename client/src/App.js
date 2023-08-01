2acdd8d2b153024158d34d9657fb94e800f097c8
Minh Tri Nguyen Vo <minhtri.nguyenvo31@gmail.com>
2023-08-02 00:55:19 +0700

    implement navbar component

--- client/src/App.js
+++ client/src/App.js
@@ -11,14 +11,19 @@ import HomePage from "./scenes/homePage";
 import ProfilePage from "./scenes/profilePage";
 
 function App() {
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
   return (
     <div className="App">
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<LoginPage />} />
-          <Route path="/home" element={<HomePage />} />
-          <Route path="/profile/:userId" element={<ProfilePage />} />
-        </Routes>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route path="/home" element={<HomePage />} />
+            <Route path="/profile/:userId" element={<ProfilePage />} />
+          </Routes>
+        </ThemeProvider>
       </BrowserRouter>
     </div>
   );
--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { Box, useMediaQuery } from "@mui/material";
+// import { useSelector } from "react-redux";
+import Navbar from "../navbar";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <Box>
+      <Navbar />
+    </Box>
+  );
 };
 
 export default HomePage;
